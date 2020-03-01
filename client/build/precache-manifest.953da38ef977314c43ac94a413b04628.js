33d603eafa15cd0d39bb26578c503f11b8e6ad06
monguis <jccnnetwork@gmail.com>
2020-02-29 20:12:18 -0700

    added new resume again

---
+++ client/build/precache-manifest.953da38ef977314c43ac94a413b04628.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0d0ed5c2987a6639747b",
+    "url": "/static/js/main.57d147a2.chunk.js"
+  },
+  {
+    "revision": "f53db7969767c4d4603e",
+    "url": "/static/js/2.f43c5a9a.chunk.js"
+  },
+  {
+    "revision": "8393d4d29349620c348322086e496d1e",
+    "url": "/index.html"
+  }
+];
