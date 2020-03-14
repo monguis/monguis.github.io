dd6740bc6992ee177261d3c4959b44ad3e0a8006
monguis <jccnnetwork@gmail.com>
2020-03-14 13:11:58 -0600

    added icons and some styling

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,18 +1,12 @@
 import React from "react"
 
 const Footer = () => (
-    <footer className="fixed-bottom bg-transparent p-0" style={{position: "sticky"}}>
-      <p>Some footer nonsen 
-          
-          
-          
-          
-          
-          
-          
-          
-          se!</p>
-    </footer>
+    <footer className="fixed-bottom bg-transparent" style={{position: "sticky"}}>
+      <i className="fab footerIcon fa-github"></i><i className="fab footerIcon fa-linkedin-in"></i><i className="fab footerIcon fa-facebook-f"></i><i className="far footerIcon fa-file"></i>
+     </footer>
   );
 
   export default Footer;
+
+  // <p>Some footer nonsen 
+  // </p>
